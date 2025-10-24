dd53168214f32216791b6e089f6df4ef8b6cac40
filipjt88 <filip88ks@gmail.com>
2025-10-24 16:19:33 +0200

    test success

---
+++ select_basketball_test.php
@@ -0,0 +1,11 @@
+<?php
+require 'db.php';
+
+$sql = "SELECT * FROM basketball_matches";
+$stmt = $pdo->query($sql);
+$matches = $stmt->fetchAll();
+
+echo "<pre>";
+print_r($matches);
+echo "</pre>";
+?>
