120fdeecb78dd4f362ac36589bcce3edae346f36
filipjt88 <filip88ks@gmail.com>
2025-10-24 19:32:51 +0200

    edit

--- db.php
+++ db.php
@@ -14,7 +14,6 @@ $options = [
 
 try {
     $pdo = new PDO($dsn, $user, $pass, $options);
-    // echo "Connection success!"; // <-- OVO UKLONI
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
