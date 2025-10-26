ee3b919640367f93bf27f41711dcb62d75fe9774
filipjt88 <filip88ks@gmail.com>
2025-10-26 20:00:47 +0100

    save

--- insert_test.php
+++ insert_test.php
@@ -2,9 +2,9 @@
 require 'db.php'; // Include PDO connect
 
 // Test data for a soccer match
-$league = "UEFA Champions League";
-$home_team = "Real Madrid";
-$away_team = "Liverpool";
+$league     = "UEFA Champions League";
+$home_team  = "Real Madrid";
+$away_team  = "Liverpool";
 $home_score = 2;
 $away_score = 1;
 $match_date = "2025-10-24 20:45";
@@ -15,7 +15,7 @@ $sql = "INSERT INTO football_matches (league, home_team, away_team, home_score, 
 $stmt = $pdo->prepare($sql);
 
 $stmt->execute([
-    ':league' => $league,
+    ':league'     => $league,
     ':home_team'  => $home_team,
     ':away_team'  => $away_team,
     ':home_score' => $home_score,
