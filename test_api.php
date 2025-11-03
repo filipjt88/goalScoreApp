ed4390c5afd9196e630145f1a00f3c64710048a0
filipjt88 <filip88ks@gmail.com>
2025-11-03 14:06:03 +0100

    test

--- test_api.php
+++ test_api.php
@@ -1,12 +1,11 @@
 <?php
-require 'db.php'; // ako želiš kasnije da ubacujemo rezultate u bazu
+require 'db.php';
 
 $apiKey   = '86a166ebe14c23ea3591d3934b47b439';
 $leagueId = 2; // Primer: UEFA Champions League (možemo promeniti)
 $season   = 2023;
 
 $url = "https://v3.football.api-sports.io/fixtures?league=$leagueId&season=$season";
-
 $curl = curl_init();
 
 curl_setopt_array($curl, [
